713dee87be06b9ecfff52a57d32c34539bab9f2e
xgd <895740313@qq.com>
2026-03-18 16:44:56 +0800

    Update robots.txt configuration to allow access to the sitemap.xml in addition to the root path, enhancing SEO and site indexing.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: ["/", "/sitemap.xml"],
       disallow: ["/settings"],
     },
     sitemap: `${base}/sitemap.xml`,
